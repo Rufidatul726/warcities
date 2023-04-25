c4df32d82d50416826896c1accf841fea542184e
Md Atiquer Rahman <35799095+AtiQ-Rahman@users.noreply.github.com>
2023-04-25 11:34:41 +0600

    Change Base URL

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
- $config['base_url'] = 'http://www.warcities.com/';
+ $config['base_url'] = 'http://sub.warcities.com/';
 
 /* $config['base_url'] = 'http://localhost:8888/warcities/';*/
 /*
